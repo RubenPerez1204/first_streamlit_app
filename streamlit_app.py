a3d72a1e7e7753fdf7871e9c0179e5f099c5190f
RubenPerez <ruben.perez@civica-soft.com>
2023-02-22 07:53:43 +0100

    Adding text input jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,5 @@ streamlit.dataframe(my_data_rows)
 
 # allow the user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Write here')
-my_cur.execute("insert into fruit_load_list select "+add_my_fruit)
+streamlit.write("Thanks for adding jackfruit")
+#my_cur.execute("insert into fruit_load_list select "+add_my_fruit)
